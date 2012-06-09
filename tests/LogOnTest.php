d2c609c3169d3a0867d5188f3d0f95b14e3cc99a
developer <developer@developer-mint>
2012-06-09 04:27:56 -0400

    Fixed compile error due to refactoring scope of target

--- tests/LogOnTest.php
+++ tests/LogOnTest.php
@@ -22,39 +22,39 @@ class LogOnTest extends PHPUnit_Framework_TestCase {
     }
     
     public function testExpectedVersion() {
-        $version = $target->getVersion();
+        $version = $this->target->getVersion();
         $this->assertEquals(1.0, $version);
     }
     
     public function testInitiate()
     {
         
-        $nonce = $target->initiate();
+        $nonce = $this->target->initiate();
         $this->assertEquals(32, strlen($nonce));
     }
     
     public function testInitiateIsUnique()
     {
-        $firstNonce = $target->initiate();
-        $secondNonce = $target->initiate();
+        $firstNonce = $this->target->initiate();
+        $secondNonce = $this->target->initiate();
         $this->assertNotEquals($firstNonce, $secondNonce);
     }
     
     public function testAuthenticate()
     {
-        $nonce = $target->initiate();
+        $nonce = $this->target->initiate();
         $cnonce = hash('md5', rand(0, getrandmax()).'MyOwnValue');
         $hash = hash('md5', "$nonce:$cnonce:$this->password");
-        $token = $target->authenticate($this->username, $cnonce, $hash);
+        $token = $this->target->authenticate($this->username, $cnonce, $hash);
         $this->assertEquals(32, strlen($token));
     }
 
     public function testAuthenticateWithBadUsername()
     {
-        $nonce = $target->initiate();
+        $nonce = $this->target->initiate();
         $cnonce = hash('md5', rand(0, getrandmax()).'MyOwnValue');
         $hash = hash('md5', "$nonce:$cnonce:$this->password");
-        $token = $target->authenticate($this->badUsername, $cnonce, $hash);
+        $token = $this->target->authenticate($this->badUsername, $cnonce, $hash);
         $this->assertEquals('', $token);
     }
 }
