68e7faf0f1554e9016e6ac5e286bb0ef415ba8ff
developer <developer@developer-mint>
2012-06-09 21:25:44 -0400

    LM - FZ3 - Added data provider to communicate with database

---
+++ DatabaseWrapper.php
@@ -0,0 +1,115 @@
+<?php
+require_once dirname(__FILE__) . '/configuration.php';
+
+/**
+ * Description of DataProvider
+ *
+ * @author developer
+ */
+class DatabaseWrapper {
+
+    private $connection;
+    private $affectedRows;
+    
+    public function getRows($query)
+    {
+       $result = $this->query($query);
+       //$rows = $result->fetch_all();
+       $rows = array();
+       while($row = $result->fetch_array())
+       {
+           $rows[] = $row;
+       }
+       $result->free();
+       return $rows;
+    }
+    
+    public function getRow($query)
+    {
+       $result = $this->query($query);
+       $row = $result->fetch_assoc();
+       $result->free();
+       return $row;
+    }
+    
+    public function getValue($query)
+    {
+        $row = $this->getRow($query);
+        if(is_array($row))
+        {
+            foreach($row as $value)
+            {
+                return $value;
+            }
+        }
+        return NULL;
+    }
+    
+    public function getFirstRow($query)
+    {
+       $this->result = $this->query($query);
+       $row = $this->result->fetch_assoc();
+       return $row;
+        
+    }
+    
+    public function getNextRow()
+    {
+       $row = $this->result->fetch_assoc();
+       if($row === NULL)
+       {
+            $this->result->free();
+            $this->result = NULL;
+       }
+       return $row;
+        
+    }
+    
+    public function execute($query)
+    {
+       $result = $this->query($query);
+       $affected = $this->affectedRows;
+       if($result && is_object($result))
+       {
+            $result->free();
+       }
+       return $affected;
+    }
+    
+    private function query($query)
+    {
+       $db = $this->getConnection();
+       $result = $db->query($query);
+
+       if($this->connection->error)
+        {
+          die($this->connection->error);
+        }
+        $this->affectedRows = $this->connection->affected_rows;
+        
+       return $result;
+    }
+    
+    private function getConnection()
+    {
+        if($this->connection == NULL)
+        {
+            $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, 
+                    DB_NAME, DB_PORT, DB_SOCKET)
+            or die ('Could not connect to the database server');
+        }
+        
+        return $this->connection;
+    }
+
+    function __destruct()
+    {
+        if($this->connection !== NULL)
+        {
+            $this->connection->close();
+            $this->connection = NULL;
+        }
+    }
+}
+
+?>
